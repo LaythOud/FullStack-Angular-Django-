407038ca9e58c1de1e8a8ca628364c9c12394710
laythoud <laythoud5555@gmail.com>
2025-01-30 20:19:07 +0400

    Fix file location and some formating

--- Django/opaala/base/urls.py
+++ Django/opaala/base/urls.py
@@ -3,11 +3,23 @@ from . import views
 
 urlpatterns = [
     # GET all books
-    path("books", views.books, name="get_all_books"),
+    path(
+        "books",
+        views.books,
+        name="get_all_books",
+    ),
     # POST, GET (create list, get all lists)
-    path("lists", views.book_lists, name="action_lists"),
+    path(
+        "lists",
+        views.book_lists,
+        name="action_lists",
+    ),
     # GET or DELETE (get or delete one list)
-    path("lists/<slug:list_slug>", views.book_list, name="action_list"),
+    path(
+        "lists/<slug:list_slug>",
+        views.book_list,
+        name="action_list",
+    ),
     # POST (add book to list)
     path(
         "lists/<slug:list_slug>/books",
